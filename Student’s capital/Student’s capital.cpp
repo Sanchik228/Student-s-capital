ecfd38e2a083815812e71790d6c84b7296b6c7ad
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-07-01 13:53:32 +0300

    finale version

--- Student’s capital/Student’s capital.cpp
+++ Student’s capital/Student’s capital.cpp
@@ -6,8 +6,9 @@ struct Laptop {
     int gain;
     int price;
     double profit;
+    int id;
 
-    Laptop(int p, int g) : price(p), gain(g) {
+    Laptop(int p, int g, int i) : price(p), gain(g), id(i) {
         profit = (double)gain / price;
     }
 };
@@ -26,7 +27,7 @@ int main() {
         int gain, price;
         std::cout << "Laptop " << (i + 1) << " ";
         std::cin >> price >> gain;
-        laptops.emplace_back(price, gain);
+        laptops.emplace_back(price, gain, i+1);
     }
 
     std::sort(laptops.begin(), laptops.end(), [](const Laptop& a, const Laptop& b) {
@@ -34,15 +35,26 @@ int main() {
         });
 
     int bought = 0, capital = C;
+    std::vector<int> boughtLap;
     for (const auto& laptop : laptops) {
         if (bought >= N) break;
         if (laptop.price <= capital) {
             capital -= laptop.price;
             capital += laptop.gain;
             bought++;
+            boughtLap.emplace_back(laptop.id);
         }
     }
     std::cout << "Capital after summer: " << capital << std::endl;
+
+    if (!boughtLap.empty()) {
+        std::cout << "Laptops bought: ";
+        for (auto id : boughtLap) std::cout << "Laptop " << id << " ";
+        std::cout << std::endl;
+    }
+    else {
+        std::cout << "No laptops could be bought." << std::endl;
+    }
 
     return 0;
 }
